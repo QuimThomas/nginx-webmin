02f8eff7b927742cfd54ce20e72734108c41aa83
QuimThomas <quimthomas@gmail.com>
2012-02-07 15:10:53 +0700

    Update virtual_feature.pl

--- virtual_feature.pl
+++ virtual_feature.pl
@@ -25,65 +25,65 @@ if($config{'sites_enabled_dir'} eq "")
 
 sub feature_always_links
 {
-  
+
 }
 
 sub feature_backup
 {
-  
+
 }
 
 sub feature_bandwidth
 {
-  
+
 }
 
 sub feature_check
 {
   my $conf_file = $conf_dir . "nginx.conf";
-  
+
   unless(-r $conf_file)
   {
     return "Nginx needs to be installed.";
   }
-  
+
   unless (-d $conf_dir . $sites_available_dir)
   {
     mkdir($conf_dir . $sites_available_dir);
   }
-  
+
   unless (-d $conf_dir . $sites_enabled_dir)
   {
     mkdir($conf_dir . $sites_enabled_dir);
   }
 
-  
+
   open(CONF, $conf_file);
-  
+
   local $/ = undef;
   my $filestring = <CONF>;
   close(CONF);
-  
+
   #TODO: check if include directive not comment "# include /etc/nginx/sites-enabled/*;"
   my $pattern = "include " . $conf_dir . $sites_enabled_dir . "\\*;";
-  
-  unless ($filestring =~ /$pattern/) 
+
+  unless ($filestring =~ /$pattern/)
   {
-    
+
     chop($filestring);
-    
-    $filestring .= "\tinclude " . $conf_dir . $sites_enabled_dir . "*;\n}"; 
-    
+
+    $filestring .= "\tinclude " . $conf_dir . $sites_enabled_dir . "*;\n}";
+
     open(CONF,  ">", $conf_file);
-    
+
     print(CONF $filestring);
-    
+
     close(CONF);
-    
+
   }
-    
+
   return undef;
-  
+
 }
 
 sub feature_clash
@@ -95,12 +95,12 @@ sub feature_delete
 {
   my ($d) = @_;
   &$virtual_server::first_print("Deleting Nginx site ..");
-  
+
   unlink($conf_dir . $sites_enabled_dir . $d->{'dom'} . ".conf");
   unlink($conf_dir . $sites_available_dir . $d->{'dom'} . ".conf");
-  
+
   &$virtual_server::second_print(".. done");
-  
+
 }
 
 sub feature_depends
@@ -124,19 +124,19 @@ sub feature_disname
 
 sub feature_enable
 {
-  
+
   my ($d) = @_;
   &$virtual_server::first_print("Re-enabling Nginx website ..");
   symlink($conf_dir . $sites_available_dir . $d->{'dom'} . ".conf", $conf_dir . $sites_enabled_dir . $d->{'dom'} . ".conf");
   reload_nginx();
   &$virtual_server::second_print(".. done");
-  
-  
+
+
 }
 
 sub feature_import
 {
-  
+
 }
 
 sub feature_label
@@ -146,7 +146,7 @@ sub feature_label
 
 sub feature_links
 {
-  
+
 }
 
 sub feature_losing
@@ -168,21 +168,21 @@ sub feature_name
 
 sub feature_restore
 {
-  
+
 }
 
 sub feature_setup
 {
   my ($d) = @_;
   &$virtual_server::first_print("Setting up Nginx site ..");
-  
+
   my $file;
-  
+
   if($config{'log_dir'} eq "")
   {
     $log_dir = "$d->{'home'}/logs/";
   }
-  
+
   open($file, ">" . $conf_dir . $sites_available_dir . $d->{'dom'} . ".conf");
   #TODO in config.info add nginx config template with default value conf_tmpl=nginx config template,9,server{ listen $d->{'ip'}:80;} or get it from nginx_conf.tpl and parse
   #TODO Determine subdomain and dont put rewrite ^/(.*) http://www.$d->{'dom'} permanent;
@@ -195,44 +195,41 @@ sub feature_setup
   server {
     listen $d->{'ip'}:80;
     server_name $d->{'dom'} www.$d->{'dom'};
-    
+    location / {
+    root $d->{'home'}/public_html/;
+    proxy_pass   http://$d->{'dom'}:1616;
+    }
+    location ~* ^.+.(jpg|png|gif|jpeg|js|css|mp3|wav|swf|mov|doc|pdf|xls|ppt|docx|pptx|xlsx)\$ {
+    root $d->{'home'}/public_html/;
+    expires           30d;
+    access_log        off;
+    }
     access_log $log_dir$d->{'dom'}.access.log;
     error_log $log_dir$d->{'dom'}.error.log;
-  
-    root $d->{'home'}/public_html/;
     index index.php index.html index.htm;
-    
-    if (!-e \$request_filename) {
-      rewrite ^/(.*)\$ /index.php?q=\$1 last;
+    #location ~ \.php\$ {
+    #  fastcgi_pass 127.0.0.1:9000;
+    #  fastcgi_index index.php;
+    #  fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+    #  include fastcgi_params;
+    #}
+    location ~* ^.+.(ini|htaccess|htpasswd|php*|php.*)\$ {
+    deny    all
     }
-    
-    # serve static files directly
-    location ~* ^.+.(jpg|jpeg|gif|css|png|js|ico)\$ {
-      access_log        off;
-      expires           30d;
-    }
-    
-    location ~ \.php\$ {
-      fastcgi_pass 127.0.0.1:9000;
-      fastcgi_index index.php;
-      fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-      include fastcgi_params;
-    }
-
   }
 CONFIG
-  
+
   print($file $conf);
-  
+
   close $file;
-  
+
   symlink($conf_dir . $sites_available_dir . $d->{'dom'} . ".conf", $conf_dir . $sites_enabled_dir . $d->{'dom'} . ".conf");
-  
+
   reload_nginx();
   fix_perm("$d->{'home'}/public_html/");
-  
+
   &$virtual_server::second_print(".. done");
-  
+
 }
 
 sub feature_suitable
@@ -247,7 +244,7 @@ sub feature_validate
 
 sub feature_webmin
 {
-  
+
 }
 
 sub reload_nginx
@@ -265,7 +262,7 @@ sub fix_perm
 {
     # TODO nginx run as www-data, default perm on public_html in Virtualmin 0740 Sequrity alert if chmod 0755
     my ($dir) = @_;
-    
+
     if($config{'public_html_perm'} eq "")
     {
         $public_html_perm = '0755';
